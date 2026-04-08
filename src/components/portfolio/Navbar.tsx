c57d65ee47ea231b492c4fb5cb28d05c141bc38d
suraj <kumar.suraj9918@gmail.com>
2026-04-09 01:02:58 +0530

    Merge branch 'main' into feature

--- src/components/portfolio/Navbar.tsx
+++ src/components/portfolio/Navbar.tsx
@@ -66,7 +66,7 @@ const Navbar = () => {
             transition={{ delay: 0.5 }}
           >
             <a
-              href="/Surajkumar_Gupta_Resume.pdf.pdf"
+              href="/Surajkumar_Gupta_Resume.pdf"
               target="_blank"
               className="font-mono text-sm px-4 py-2 border border-primary text-primary rounded hover:bg-primary/10 transition-all"
             >
